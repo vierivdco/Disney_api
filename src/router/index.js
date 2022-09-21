18df83b77da7944a47bcb68ed119aaf5a08a21fa
Vieri <vieri.901@gmail.com>
2022-09-21 17:25:19 -0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { TestRouter, UserRouter } from "../components";
+
+// cada vez que quiera agregar una ruta nueva,
+// creo el path e importo el componente
+const listRoutes = [
+  ["/test", TestRouter],
+  ["/user", UserRouter],
+];
+
+export const routes = (app) => {
+  listRoutes.forEach(([path, controller]) => {
+    app.use(path, controller);
+  });
+};
